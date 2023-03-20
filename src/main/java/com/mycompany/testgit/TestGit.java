dddc74f880c645d01e811ea7100cfca2fb52d6e2
=fporteroZV <franciscoportero@ieszaidinvergeles.org>
2023-03-20 20:40:28 +0100

    segundo commit

--- src/main/java/com/mycompany/testgit/TestGit.java
+++ src/main/java/com/mycompany/testgit/TestGit.java
@@ -12,6 +12,8 @@ package com.mycompany.testgit;
 public class TestGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!2");
+        System.out.println("nueva linea");
+        
     }
 }
